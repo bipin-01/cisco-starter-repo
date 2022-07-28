53ef40c995954b169134e97a8044f15189f29e12
bipin-01 <poudel7700@gmail.com>
2022-07-28 23:53:29 +0545

    deleted unwanted node module folder

--- sextant/src/components/Banner.js
+++ sextant/src/components/Banner.js
@@ -1,7 +1,7 @@
 export default function Banner () {
     return (
         <div>
-        <h1 style={{textAlign: "center", backgroundColor:'blue', height:'50px'}}>Sextant App</h1>
+        <h1 style={{textAlign: "center", backgroundColor: '#99ffcc', height:'50px'}}>Sextant App</h1>
       </div>
     )
 }
