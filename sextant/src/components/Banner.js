711ad17510c48bb205fb30a8843b3f021ae646fd
bipin-01 <poudel7700@gmail.com>
2022-07-28 23:45:54 +0545

    Add Banner component to Sextant App

---
+++ sextant/src/components/Banner.js
@@ -0,0 +1,9 @@
+export default function Banner () {
+    return (
+        <div>
+        <h1 style={{textAlign: "center", backgroundColor:'blue', height:'50px'}}>Sextant App</h1>
+      </div>
+    )
+}
+
+
